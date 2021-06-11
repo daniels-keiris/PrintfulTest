36218d27fb2254f2bf359f0e9cb49814fde49839
daniels-keiris <daniels.keiris@gmail.com>
2021-06-11 15:38:54 +0300

    Removed unnecessary comments

--- index.php
+++ index.php
@@ -2,11 +2,7 @@
 
 require ('vendor/autoload.php');
 
-// $loader = require 'vendor/autoload.php';
-// $loader->addPsr4('', __DIR__.'/../src/');
-
 use Controllers\TaskController;
-// use Models\Task;
 
 $taskController = new TaskController();
 $taskController->taskList();
